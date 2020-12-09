dbbfc471097ac31cfd711633a18e5fc9f55a803e
feng26951 <l_feng_11@163.com>
2020-12-09 09:54:18 +0800

    qqq

---
+++ app/src/main/java/com/aiqidian/myapplication/Aasdds.java
@@ -0,0 +1,11 @@
+package com.aiqidian.myapplication;
+
+/**
+ * @author: 浊酒忆悲
+ * @date: 2020/12/9
+ * @package_name: com.aiqidian.myapplication
+ * @E-mail: l_feng_11@163.com
+ * @Description:
+ */
+public class Aasdds {
+}
--- app/src/main/java/com/aiqidian/myapplication/MainActivity.java
+++ app/src/main/java/com/aiqidian/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.aiqidian.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "1563", Toast.LENGTH_SHORT).show();
 
 
     }
